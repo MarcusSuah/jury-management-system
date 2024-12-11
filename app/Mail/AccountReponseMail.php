e6b5a9ae4c4d6afd03e15c83a205034e4f989d77
MarcusSuah <smarcus.suah@gmail.com>
2024-12-11 16:40:51 +0200

    More Update

---
+++ app/Mail/AccountReponseMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class AccountReponseMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    public function build()
+    {
+        return $this->subject("Judge Account Application Response")->view("panel.emails.account-mail");
+    }
+}
--- resources/views/panel/questionnaire/quiz.blade.php
+++ resources/views/panel/questionnaire/quiz.blade.php
@@ -34,42 +34,46 @@
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
-   $(document).ready(function () {
-    let timer;
-    // 10 minutes in milliseconds
-    const quizDuration = 10 * 60 * 1000; 
-    let timeRemaining = quizDuration;
-    let currentQuestionIndex = 0;
-    let questions = [];
-    let score = 0;
-    let assignmentID = 0;
-    const results = { status: '', score: 0, assignmentID: assignmentID };
-    let biasedCount = 0;
+    $(document).ready(function() {
+        let timer;
+        // 10 minutes in milliseconds
+        const quizDuration = 10 * 60 * 1000;
+        let timeRemaining = quizDuration;
+        let currentQuestionIndex = 0;
+        let questions = [];
+        let score = 0;
+        let assignmentID = 0;
+        const results = {
+            status: '',
+            score: 0,
+            assignmentID: assignmentID
+        };
+        let biasedCount = 0;
 
-    function startTimer() {
-        timer = setInterval(function () {
-            timeRemaining -= 1000;
-            const minutes = Math.floor(timeRemaining / (60 * 1000));
-            const seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000);
-            $('#timer').text(`Time Left: ${minutes}m ${seconds}s`);
+        function startTimer() {
+            timer = setInterval(function() {
+                timeRemaining -= 1000;
+                const minutes = Math.floor(timeRemaining / (60 * 1000));
+                const seconds = Math.floor((timeRemaining % (60 * 1000)) / 1000);
+                $('#timer').text(`Time Left: ${minutes}m ${seconds}s`);
 
-            if (timeRemaining <= 0) {
-                clearInterval(timer);
-                submitResult('fail', 'Time Up');
-            }
-        }, 1000);
-    }
+                if (timeRemaining <= 0) {
+                    clearInterval(timer);
+                    submitResult('fail', 'Time Up');
+                }
+            }, 1000);
+        }
 
-    function updateQuestionCount() {
-        $('#question-count').text(`Question ${currentQuestionIndex + 1} of ${questions.length}`);
-    }
+        function updateQuestionCount() {
+            $('#question-count').text(`Question ${currentQuestionIndex + 1} of ${questions.length}`);
+        }
 
-    function loadQuestion(index) {
-        const question = questions[index];
-        const $questionContainer = $('#question-container');
+        function loadQuestion(index) {
+            const question = questions[index];
+            const $questionContainer = $('#question-container');
 
-        if (question) {
-            let answersHtml = `
+            if (question) {
+                let answersHtml = `
                 <h4>${question.question}</h4>
                 <div>
                     <label>
@@ -83,104 +87,107 @@
                 </div>
             `;
 
-            question.answers.wrong.forEach((wrongAnswer, i) => {
-                answersHtml += `
+                question.answers.wrong.forEach((wrongAnswer, i) => {
+                    answersHtml += `
                     <div>
                         <label>
                             <input type="radio" name="answer" value="wrong-${i}"> ${wrongAnswer}
                         </label>
                     </div>
                 `;
-            });
+                });
 
-            $questionContainer.html(answersHtml);
+                $questionContainer.html(answersHtml);
 
-            // Enable "Next" button when an answer is selected
-            $('input[name="answer"]').on('change', function () {
-                $('#next-button').prop('disabled', false);
-            });
+                // Enable "Next" button when an answer is selected
+                $('input[name="answer"]').on('change', function() {
+                    $('#next-button').prop('disabled', false);
+                });
 
-            // Update the question count
-            updateQuestionCount();
-        } else {
-            submitResult('completed', 'Quiz Completed');
-        }
-    }
-
-    function submitResult(quizResult, message) {
-        clearInterval(timer);
-        if(quizResult === 'completed'){
-            if(score > 6){
-                results.status = 'pass';
-            }else{
-                results.status = 'fail';
+                // Update the question count
+                updateQuestionCount();
+            } else {
+                submitResult('completed', 'Quiz Completed');
             }
-        }else{
-            results.status = quizResult;
         }
-        
-        results.score = score;
-        results.assignmentID = assignmentID;
-        alert(`${message}\nYour Score: ${score}/${questions.length}`);
-        
-        // Send the result to the server
+
+        function submitResult(quizResult, message) {
+            clearInterval(timer);
+            if (quizResult === 'completed') {
+                if (score > 6) {
+                    results.status = 'pass';
+                } else {
+                    results.status = 'fail';
+                }
+            } else {
+                results.status = quizResult;
+            }
+
+            results.score = score;
+            results.assignmentID = assignmentID;
+            alert(`${message}\nYour Score: ${score}/${questions.length}`);
+
+            // Send the result to the server
+            $.ajax({
+                type: 'POST',
+                url: '/panel/submit-result',
+                data: {
+                    ...results,
+                    _token: '{{ csrf_token() }}'
+                },
+                success: function() {
+                    window.location.href = "/panel/quiz-records";
+                },
+                error: function() {
+                    alert('Failed to submit the result. Please try again.');
+                }
+            });
+        }
+
+        function handleAnswer(selectedAnswer) {
+            // if (selectedAnswer.startsWith('wrong')) {
+            //     submitResult('fail', 'Oops! You cannot continue this quiz.');
+            //     return;
+            // }
+            if (selectedAnswer === 'biased') {
+                biasedCount = biasedCount + 1;
+                if (biasedCount == 2) {
+                    submitResult('biased', 'Oops! You cannot continue this quiz.');
+                    return;
+                }
+            }
+
+            // Increment score for correct answers
+            if (selectedAnswer === 'true') {
+                score++;
+            }
+
+            // Move to the next question
+            currentQuestionIndex++;
+            loadQuestion(currentQuestionIndex);
+
+            // Disable "Next" button until a new answer is selected
+            $('#next-button').prop('disabled', true);
+        }
+
+        // Load questions via AJAX
         $.ajax({
-            type: 'POST',
-            url: '/panel/submit-result',
-            data: { ...results, _token: '{{ csrf_token() }}' },
-            success: function () {
-               window.location.href = "/panel/quiz-records";
+            type: 'GET',
+            url: "/panel/get-quiz",
+            success: function(data) {
+                questions = jQuery.parseJSON(data);
+                assignmentID = questions[0].assignment_id;
+                loadQuestion(currentQuestionIndex);
+                startTimer();
             },
-            error: function () {
-                alert('Failed to submit the result. Please try again.');
+            error: function() {
+                alert('Failed to load quiz data. Please try again later.');
             }
         });
-    }
 
-    function handleAnswer(selectedAnswer) {
-        // if (selectedAnswer.startsWith('wrong')) {
-        //     submitResult('fail', 'Oops! You cannot continue this quiz.');
-        //     return;
-        // }
-        if (selectedAnswer === 'biased') {
-            biasedCount = biasedCount + 1;
-            if(biasedCount == 2){
-                submitResult('biased', 'Oops! You cannot continue this quiz.');
-                return;
-            }
-        }
-
-        // Increment score for correct answers
-        if (selectedAnswer === 'true') {
-            score++;
-        }
-
-        // Move to the next question
-        currentQuestionIndex++;
-        loadQuestion(currentQuestionIndex);
-
-        // Disable "Next" button until a new answer is selected
-        $('#next-button').prop('disabled', true);
-    }
-
-    // Load questions via AJAX
-    $.ajax({
-        type: 'GET',
-        url: "/panel/get-quiz",
-        success: function (data) {
-            questions = jQuery.parseJSON(data);
-            assignmentID = questions[0].assignment_id;
-            loadQuestion(currentQuestionIndex);
-            startTimer();
-        },
-        error: function () {
-            alert('Failed to load quiz data. Please try again later.');
-        }
+        $('#next-button').on('click', function() {
+            const selectedAnswer = $('input[name="answer"]:checked').val();
+            handleAnswer(selectedAnswer);
+        });
     });
-
-    $('#next-button').on('click', function () {
-        const selectedAnswer = $('input[name="answer"]:checked').val();
-        handleAnswer(selectedAnswer);
-    });
-});
 </script>
